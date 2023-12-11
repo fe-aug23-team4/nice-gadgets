694483f4c6bdc3937cab8feab8a0bac38bf15666
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-11 12:07:02 +0200

    add grid and static file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { Root } from './Root';
+import { store } from './store/store';
 
-createRoot(document.getElementById('root') as HTMLDivElement)
-  .render(<Root />);
+createRoot(document.getElementById('root') as HTMLDivElement).render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+);
