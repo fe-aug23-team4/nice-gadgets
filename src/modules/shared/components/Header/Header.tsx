50f7d7cb5e827bd815bfccb9c52f291bdbd45c7f
Denys Shurubura <denys.shurubura@gmail.com>
2023-12-11 20:35:50 +0200

    add header

--- src/modules/shared/components/Header/Header.tsx
+++ src/modules/shared/components/Header/Header.tsx
@@ -1,9 +1,80 @@
-import './Header.module.scss';
-
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import cn from 'classnames';
+
+import styles from './Header.module.scss';
+import logo from '../../../../static/img/Logo.png';
+import burger from './static/icons/Burger.png';
+import favourites from './static/icons/Favourites.png';
+import cart from './static/icons/cart.png';
 
 export const Header: React.FC = () => {
+  const getLinkClass = (
+    { isActive }: { isActive: boolean },
+  ) => cn([styles.header__link], {
+    [styles.header__link__ISACTIVE]: isActive,
+  });
+
   return (
-    <h1>Header</h1>
+    <header className={styles.header}>
+      <div className={styles.header__left}>
+        <Link to="/">
+          <img src={logo} alt="logo" className={styles.header__logo} />
+        </Link>
+
+        <ul className={styles.header__nav}>
+          <li>
+            <NavLink
+              to="/"
+              className={getLinkClass}
+            >
+              home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/phones"
+              className={getLinkClass}
+            >
+              phones
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/tablets"
+              className={getLinkClass}
+            >
+              tablets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/accessories"
+              className={getLinkClass}
+            >
+              accessories
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.header__right}>
+        <div className={styles.header__icon}>
+          <NavLink to="/favorites">
+            <img src={favourites} alt="favourites" />
+          </NavLink>
+        </div>
+
+        <div className={styles.header__icon}>
+          <NavLink to="/cart">
+            <img src={cart} alt="cart" />
+          </NavLink>
+        </div>
+
+        <div className={styles.header__burger}>
+          <img src={burger} alt="burger-menu" />
+        </div>
+      </div>
+    </header>
   );
 };
