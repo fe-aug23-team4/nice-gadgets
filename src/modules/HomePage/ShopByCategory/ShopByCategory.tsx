789296686aca4d78f53f2cfe1ba757ed7d22d771
OlhaKozuik <gadyakaban@gmail.com>
2023-12-11 12:43:33 +0200

    add LF

--- src/modules/HomePage/ShopByCategory/ShopByCategory.tsx
+++ src/modules/HomePage/ShopByCategory/ShopByCategory.tsx
@@ -6,14 +6,17 @@ export const ShopByCategory: React.FC = () => {
     <div>
       <h2>Shop by category</h2>
       <article>
+        <img src="" alt="" />
         <h3>Mobile phones</h3>
         <p>95 models</p>
       </article>
       <article>
+        <img src="" alt="" />
         <h3>Tablets</h3>
         <p>24 models</p>
       </article>
       <article>
+        <img src="" alt="" />
         <h3>Accessories</h3>
         <p>100 models</p>
       </article>
