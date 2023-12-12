bbc02122177afb47f1b82a43ba7bab6a44653b68
drewetstreiber <miffesy@gmail.com>
2023-12-12 15:08:50 +0100

    made the changes

--- src/modules/shared/components/Footer/Footer.tsx
+++ src/modules/shared/components/Footer/Footer.tsx
@@ -8,38 +8,36 @@ export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <nav className={styles.nav}>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="nice_gadgets_logo"
+            className={styles.nav_logo}
+          />
+        </Link>
+        <div className={styles.nav_center}>
+          <Link to="/github" className={styles.nav_text}>
+            Github
+          </Link>
+          <Link to="/contacts" className={styles.nav_text}>
+            Contacts
+          </Link>
+          <Link to="/rights" className={styles.nav_text}>
+            Rights
+          </Link>
+        </div>
+        <div className={styles.return_button}>
+          <Link to="#top" className={styles.button_top}>
+            Back to Top
+          </Link>
           <Link to="/">
             <img
-              src={logo}
+              src={button}
               alt="nice_gadgets_logo"
-              className={styles.nav_logo}
+              className={styles.button_logo}
             />
           </Link>
-          <div className={styles.nav_center}>
-            <Link to="/github" className={styles.nav_text}>
-              Github
-            </Link>
-            <Link to="/contacts" className={styles.nav_text}>
-              Contacts
-            </Link>
-            <Link to="/rights" className={styles.nav_text}>
-              Rights
-            </Link>
-          </div>
-          <div className="return_button">
-            <Link to="#top" className={styles.button_top}>
-              Back to Top
-            </Link>
-            <Link to="/">
-              <img
-                src={button}
-                alt="nice_gadgets_logo"
-                className={styles.button_logo}
-              />
-            </Link>
-          </div>
-        </nav>
+        </div>
       </div>
     </footer>
   );
