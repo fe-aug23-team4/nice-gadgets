60ad9c2a73ca12506840d7cb7b463c001785ab13
Vika Levik <levika002@gmail.com>
2023-12-13 14:15:45 +0200

    slider with promo firt attemp

--- src/modules/HomePage/components/SliderWithPromoImages/SliderWIthPromo.tsx
+++ src/modules/HomePage/components/SliderWithPromoImages/SliderWIthPromo.tsx
@@ -1,10 +1,44 @@
-import './SliderWithPromo.module.scss';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 
 import React from 'react';
 import Slider from 'react-slick';
+import cn from 'classnames';
 
-import './slick.css';
-import './slick-theme.css';
+import './slick.scss';
+import './slick-theme.scss';
+
+import { bannersData } from '../../../../static/banners/bannersData';
+import style from './SliderWithPromo.module.scss';
+import { useAppSelector } from '../../../../store/hooks';
+
+function SampleNextArrow(props: any) {
+  const { className, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+      onKeyDown={onClick}
+      role="button"
+      tabIndex={-1}
+    />
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  const { className, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+      onKeyDown={onClick}
+      role="button"
+      tabIndex={-1}
+    />
+  );
+}
 
 export const SliderWithPromo: React.FC = () => {
   const settings = {
@@ -17,30 +51,36 @@ export const SliderWithPromo: React.FC = () => {
     autoplaySpeed: 5000,
     pauseOnHover: true,
     swipeToSlide: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    adaptiveHeight: true,
   };
 
+  const { isDarkTheme } = useAppSelector((state) => state.theme);
+
   return (
-    <div>
-      <h2> Single Item</h2>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+    <div className={style.container}>
+      <h1 className={cn(style.header, {
+        [style.header__dark]: isDarkTheme,
+      })}
+      >
+        Welcome to Nice Gadgets store!
+      </h1>
+      <Slider
+        {...settings}
+        className={cn(style.slider, {
+          [style.slider__dark]: isDarkTheme,
+        })}
+      >
+        {bannersData.map(banner => (
+          <div key={banner.id}>
+            <img
+              src={process.env.PUBLIC_URL + banner.photo}
+              alt={banner.title}
+              className={style.sliderPhoto}
+            />
+          </div>
+        ))}
       </Slider>
     </div>
   );
