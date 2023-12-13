c3f3961903b0aaa39e7688f0856a0725e7feda75
Vika Levik <levika002@gmail.com>
2023-12-13 21:35:36 +0200

    fixed styles

--- src/modules/HomePage/components/SliderWithPromoImages/SliderWIthPromo.tsx
+++ src/modules/HomePage/components/SliderWithPromoImages/SliderWIthPromo.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
 import Slider from 'react-slick';
 import cn from 'classnames';
 
@@ -11,36 +8,13 @@ import './slick-theme.scss';
 import { bannersData } from '../../../../static/banners/bannersData';
 import style from './SliderWithPromo.module.scss';
 import { useAppSelector } from '../../../../store/hooks';
-
-function SampleNextArrow(props: any) {
-  const { className, onClick } = props;
-
-  return (
-    <div
-      className={className}
-      onClick={onClick}
-      onKeyDown={onClick}
-      role="button"
-      tabIndex={-1}
-    />
-  );
-}
-
-function SamplePrevArrow(props: any) {
-  const { className, onClick } = props;
-
-  return (
-    <div
-      className={className}
-      onClick={onClick}
-      onKeyDown={onClick}
-      role="button"
-      tabIndex={-1}
-    />
-  );
-}
+import { SampleNextArrow } from './SampleNextArrow';
+import { SamplePrevArrow } from './SamplePrevArrow';
 
 export const SliderWithPromo: React.FC = () => {
+  const ref = createRef<HTMLDivElement>();
+
+  const { isDarkTheme } = useAppSelector((state) => state.theme);
   const settings = {
     dots: true,
     infinite: true,
@@ -54,9 +28,22 @@ export const SliderWithPromo: React.FC = () => {
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
     adaptiveHeight: true,
+    appendDots: (dots: any) => (
+      <div
+        ref={ref}
+      >
+        <ul>
+          {dots}
+        </ul>
+      </div>
+    ),
   };
 
-  const { isDarkTheme } = useAppSelector((state) => state.theme);
+  useEffect(() => {
+    if (ref.current && isDarkTheme) {
+      ref.current.className = 'slick-dots slick-dots__dark';
+    }
+  }, [ref, isDarkTheme]);
 
   return (
     <div className={style.container}>
