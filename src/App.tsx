f0b70b36a14a0822eb88f6e44713796b29ba9d78
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-16 01:24:02 +0200

    add page markup

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export const App: React.FC = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="container">
         <Outlet />
       </main>
       <Footer />
--- src/modules/HomePage/componets/MainTitle/MainTitle.tsx
+++ src/modules/HomePage/componets/MainTitle/MainTitle.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './MainTitle.module.scss';
 
 export const MainTitle: React.FC = () => (
-  <h1 className={styles.mainTitle}>Product Catalog</h1>
+  <h1 className={`page__title ${styles.mainTitle}`}>Product Catalog</h1>
 );
