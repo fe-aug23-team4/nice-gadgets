4409fec1ccbe5346655e38e4b9fcff5245d229bf
Olha <108747215+OlhaKoziuk@users.noreply.github.com>
2023-12-15 17:50:52 +0200

    first markable of cart+styles

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -1,23 +1,85 @@
-import './CartPage.module.scss';
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import cn from 'classnames';
 import { CartItem } from './CartItem';
 import { Phone } from '../../types/Phone';
 import { getNewestPhones } from '../../api/service';
+import styles from './CartPage.module.scss';
+import { useAppSelector } from '../../store/hooks';
 
 export const CartPage: React.FC = () => {
-  const [phone, setPhone] = useState<Phone | null>(null);
+  const [phones, setPhones] = useState<Phone[] | []>([]);
+  const { isDarkTheme } = useAppSelector(state => state.theme);
+
+  const total = useMemo(() => {
+    return phones.reduce((accumulator, phone) => accumulator + phone.price, 0);
+  }, [phones]);
 
   useEffect(() => {
     getNewestPhones()
-      .then(phones => setPhone(phones[0]))
+      .then(setPhones)
       // eslint-disable-next-line
       .catch((error) => console.error('Error fetching data:', error))
   }, []);
 
   return (
-    <CartItem
-      phone={phone}
-    />
+    <section
+      // className={styles.cart}
+      className={cn(styles.cart, {
+        [styles.cart__DARK]: isDarkTheme,
+      })}
+    >
+      <div
+        // className={styles.breadcrums}
+        className={cn(styles.breadcrums, {
+          [styles.contentDark]: isDarkTheme,
+        })}
+      >
+        Breadcrums
+      </div>
+
+      <h2
+        // className={styles.title}
+        className={cn(styles.title, {
+          [styles.contentDark]: isDarkTheme,
+        })}
+      >
+        Cart
+      </h2>
+
+      <div className={styles.gridContainer}>
+        <div className={styles.cardsContainer}>
+          {phones.map((phone) => (
+            <CartItem key={phone.id} phone={phone} />
+          ))}
+        </div>
+
+        <div className={styles.amountContainer}>
+          <p
+            // className={styles.totalAmount}
+            className={cn(styles.totalAmount, {
+              [styles.contentDark]: isDarkTheme,
+            })}
+          >
+            {`$ ${total}`}
+          </p>
+
+          <p
+            // className={styles.amountContent}
+            className={cn(styles.amountContent, {
+              [styles.amountContent__DARK]: isDarkTheme,
+            })}
+          >
+            {`Total for ${phones.length} items`}
+          </p>
+
+          <p className={styles.line} />
+
+          <button type="button">
+            Checkout
+          </button>
+        </div>
+      </div>
+    </section>
+
   );
 };
