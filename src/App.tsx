222a3feb7f17fff0600e7efc81cf4b600e983261
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-16 02:05:14 +0200

    dark bg + theme in localStorage
    Add dark background to pages and save theme state to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.scss';
 
 import React from 'react';
+import cn from 'classnames';
 import { Outlet } from 'react-router-dom';
 
 import { Header } from './modules/shared/Header';
 import { Footer } from './modules/shared/Footer';
+import { useAppSelector } from './store/hooks';
 
 export const App: React.FC = () => {
+  const { isDarkTheme } = useAppSelector(state => state.theme);
+
   return (
     <>
       <Header />
-      <main className="container">
+      <main
+        className={cn('container', {
+          container__DARK: isDarkTheme,
+        })}
+      >
         <Outlet />
       </main>
       <Footer />
