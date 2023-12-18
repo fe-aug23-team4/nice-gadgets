23ef23c035a586b3bdb15361de33219ac43d7587
Olha <108747215+OlhaKoziuk@users.noreply.github.com>
2023-12-18 19:09:07 +0200

    add modal window

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -1,13 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import cn from 'classnames';
 import { CartItem } from './components/CartItem';
 import styles from './CartPage.module.scss';
 import { useAppSelector } from '../../store/hooks';
 import { BackButton } from '../shared/BackButton';
+import { Modal } from './components/Modal';
 
 export const CartPage: React.FC = () => {
   const { isDarkTheme } = useAppSelector((state) => state.theme);
   const { cart } = useAppSelector(state => state.cart);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   const totalPrice = useMemo(() => {
     return cart.reduce((acc, phone) => acc + phone.price * phone.amount, 0);
@@ -18,13 +28,13 @@ export const CartPage: React.FC = () => {
   }, [cart]);
 
   return (
+
     <section
       className={cn(styles.cart, {
         [styles.cart__DARK]: isDarkTheme,
       })}
     >
       <BackButton />
-
       <h2
         className={cn(styles.title, {
           [styles.contentDark]: isDarkTheme,
@@ -75,9 +85,15 @@ export const CartPage: React.FC = () => {
             className={cn(styles.button, {
               [styles.button__DARK]: isDarkTheme,
             })}
+            onClick={openModal}
           >
             Checkout
           </button>
+          <Modal
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            cart={cart}
+          />
         </div>
       </div>
     </section>
---
+++ src/modules/CartPage/components/Modal/Modal.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import cn from 'classnames';
+import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
+import { actions as cartActions } from '../../../../store/reducers/cartSlice';
+import styles from './Modal.module.scss';
+import smile from './img/thinking-smile.png';
+import { PhoneWithAmount } from '../../../../types/Phone';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  cart: PhoneWithAmount[] | [],
+};
+
+export const Modal: React.FC<Props> = ({
+  isOpen,
+  onClose,
+  cart,
+}) => {
+  const dispatch = useAppDispatch();
+  const { isDarkTheme } = useAppSelector((state) => state.theme);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const clearCartHandler = (cartItems: PhoneWithAmount[]) => {
+    cartItems.forEach(item => dispatch(cartActions.remove(item.id)));
+    onClose();
+  };
+
+  return (
+    <div className={styles.modal}>
+      <div
+        className={cn(styles.content, {
+          [styles.content__DARK]: isDarkTheme,
+        })}
+      >
+        <p>Would you like to place an order?</p>
+        <img
+          src={smile}
+          alt="thinking-smile"
+          className={styles.smile}
+        />
+        <div className={styles.buttonsContainer}>
+          <button
+            type="button"
+            className={cn(styles.modalButton, {
+              [styles.modalButton__DARK]: isDarkTheme,
+            })}
+            onClick={() => clearCartHandler(cart)}
+          >
+            Yes
+          </button>
+          <button
+            type="button"
+            onClick={onClose}
+            className={cn(styles.modalButton, {
+              [styles.modalButton__DARK]: isDarkTheme,
+            })}
+          >
+            No
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
