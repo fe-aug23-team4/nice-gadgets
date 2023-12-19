5f941320840fa845b17b23a6208c946aae972a65
Olha <108747215+OlhaKoziuk@users.noreply.github.com>
2023-12-19 15:13:58 +0200

    add empty pages+ add hover on cartItem button

--- src/modules/FavoritesPage/FavoritesPage.tsx
+++ src/modules/FavoritesPage/FavoritesPage.tsx
@@ -4,30 +4,37 @@ import { useAppSelector } from '../../store/hooks';
 import styles from './FavoritesPage.module.scss';
 import { ProductList } from '../shared/ProductList';
 import { Breadcrumbs } from '../shared/Breadcrumbs';
+import { EmptyFavourites } from '../shared/EmptyFavourites';
 
 export const FavoritesPage: React.FC = () => {
   const { isDarkTheme } = useAppSelector((state) => state.theme);
   const { favorites } = useAppSelector(state => state.favorites);
+  const isEmptyFavorites = favorites.length === 0;
 
-  return (
-    <section
-      className={cn(styles.favouritesPage, {
-        [styles.favouritesPageDark]: isDarkTheme,
-      })}
-    >
-      <Breadcrumbs />
-      <h2
-        className={cn(styles.favouritesPage__title, {
-          [styles.favouritesPage__title__dark]: isDarkTheme,
+  return (isEmptyFavorites
+    ? <EmptyFavourites />
+    : (
+      <section
+        className={cn(styles.favouritesPage, {
+          [styles.favouritesPageDark]: isDarkTheme,
         })}
       >
-        Favourites
-      </h2>
-      <p className={styles.favouritesPage__content}>
-        {`${favorites.length || 0} items`}
-      </p>
 
-      <ProductList phones={favorites} />
-    </section>
+        <Breadcrumbs />
+        <h2
+          className={cn(styles.favouritesPage__title, {
+            [styles.favouritesPage__title__dark]: isDarkTheme,
+          })}
+        >
+          Favourites
+        </h2>
+        <p className={styles.favouritesPage__content}>
+          {`${favorites.length || 0} items`}
+        </p>
+
+        <ProductList phones={favorites} />
+      </section>
+    )
+
   );
 };
