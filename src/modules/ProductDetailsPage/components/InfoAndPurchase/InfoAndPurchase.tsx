2904e078d86ceab6468521f71b5214246a5ee41b
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-20 15:49:57 +0200

    Variants \ actions block

---
+++ src/modules/ProductDetailsPage/components/InfoAndPurchase/InfoAndPurchase.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import cn from 'classnames';
+
+import styles from './InfoAndPurchase.module.scss';
+
+import { useAppSelector } from '../../../../store/hooks';
+import { PreparedInfo, Product } from '../../../../types/Product';
+
+import { AddToCart } from '../../../shared/AddToCart';
+import { AddToFavourites } from '../../../shared/AddToFavourites';
+
+type Props = {
+  product: Product | null;
+  info: PreparedInfo;
+};
+
+export const InfoAndPurchase: React.FC<Props> = ({ product, info }) => {
+  const { isDarkTheme } = useAppSelector((state) => state.theme);
+  const { fullPrice, price, specs } = info; // eslint-disable-line object-curly-newline
+
+  return (
+    <div
+      className={cn(styles.infoAndPurchase, {
+        [styles.infoAndPurchase__DARK]: isDarkTheme,
+      })}
+    >
+      <div className={styles.infoAndPurchase__price}>
+        <p>{`$${price}`}</p>
+        <p className={styles.infoAndPurchase__fullPrice}>{`$${fullPrice}`}</p>
+      </div>
+
+      <div className={styles.infoAndPurchase__buttons}>
+        {product && (
+          <>
+            <AddToCart productItem={product} />
+            <AddToFavourites productItem={product} />
+          </>
+        )}
+      </div>
+
+      <div className={styles.infoAndPurchase__specs}>
+        {Object.entries(specs).map(([key, value]) => (
+          <div key={key} className={styles.infoAndPurchase__spec}>
+            <p className={styles.infoAndPurchase__spec__title}>{key}</p>
+            <p className={styles.infoAndPurchase__spec__text}>{value}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
